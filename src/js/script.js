3190c37f5e41c11925b62c34d2ca11f954740e46
Matthew Anthony <misterbaboo@gmail.com>
2022-07-09 17:30:30 -0400

    Change white font color to teal

--- src/js/script.js
+++ src/js/script.js
@@ -5,21 +5,21 @@ function toggleLights() {
     let bkgDoc = document.getElementById("html-background");
     let bkgClasses = bkgDoc.classList;
 
-    let textEleLight = document.getElementsByClassName("has-text-light");
+    let textEleTeal = document.getElementsByClassName("has-text-teal");
     let textEleDark = document.getElementsByClassName("has-text-dark");
 
     if (bulbClasses == 'fas fa-lightbulb has-text-dark') {
         bulbClasses.replace('fas','far');
         bkgClasses.replace('has-light-bkg','has-dark-bkg');
         Array.from(textEleDark).forEach(i => {
-            i.classList.replace('has-text-dark','has-text-light');
+            i.classList.replace('has-text-dark','has-text-teal');
         });
     }
-    else if (bulbClasses == 'far fa-lightbulb has-text-light') {
+    else if (bulbClasses == 'far fa-lightbulb has-text-teal') {
         bulbClasses.replace('far','fas');
         bkgClasses.replace('has-dark-bkg','has-light-bkg');
-        Array.from(textEleLight).forEach(i => {
-            i.classList.replace('has-text-light','has-text-dark');
+        Array.from(textEleTeal).forEach(i => {
+            i.classList.replace('has-text-teal','has-text-dark');
         });
     }
 }
