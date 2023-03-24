fc1069ccc567f6cce955d1dbe6565869525a2435
Matthew Anthony <misterbaboo@gmail.com>
2023-03-24 02:07:25 -0400

    Fix sun/moon not loading properly on refresh

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', async function() {
+document.addEventListener('DOMContentLoaded', async function () {
     if (document.cookie == 'colormode=teal') {
         document.getElementById('sun-moon').classList.add('has-text-dark');
         document.getElementById('html-background').classList.add('has-teal-bkg');
+        document.getElementById('sun-moon').classList.add('fa-moon');
 
         let titles = document.getElementsByClassName('title');
         Array.from(titles).forEach(i => {
@@ -20,6 +21,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     } else if (document.cookie == 'colormode=dark') {
         document.getElementById('sun-moon').classList.add('has-text-teal');
         document.getElementById('html-background').classList.add('has-dark-bkg');
+        document.getElementById('sun-moon').classList.add('fa-sun');
 
         let titles = document.getElementsByClassName('title');
         Array.from(titles).forEach(i => {
@@ -38,6 +40,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     } else {
         document.getElementById('sun-moon').classList.add('has-text-dark');
         document.getElementById('html-background').classList.add('has-teal-bkg');
+        document.getElementById('sun-moon').classList.replace('fa-moon');
 
         let titles = document.getElementsByClassName('title');
         Array.from(titles).forEach(i => {
@@ -59,44 +62,44 @@ document.addEventListener('DOMContentLoaded', async function() {
 
 function toggleLightDark() {
     if (document.cookie == 'colormode=teal') {
-        document.getElementById('sun-moon').classList.replace('has-text-dark','has-text-teal');
-        document.getElementById('sun-moon').classList.replace('fa-moon','fa-sun');
-        document.getElementById('html-background').classList.replace('has-teal-bkg','has-dark-bkg');
+        document.getElementById('sun-moon').classList.replace('has-text-dark', 'has-text-teal');
+        document.getElementById('sun-moon').classList.replace('fa-moon', 'fa-sun');
+        document.getElementById('html-background').classList.replace('has-teal-bkg', 'has-dark-bkg');
 
         let titles = document.getElementsByClassName('title');
         Array.from(titles).forEach(i => {
-            i.classList.replace('has-text-dark','has-text-teal');
+            i.classList.replace('has-text-dark', 'has-text-teal');
         });
 
         let subtitles = document.getElementsByClassName('subtitle');
         Array.from(subtitles).forEach(i => {
-            i.classList.replace('has-text-dark','has-text-teal');
+            i.classList.replace('has-text-dark', 'has-text-teal');
         });
 
         let icons = document.getElementsByClassName('fab');
         Array.from(icons).forEach(i => {
-            i.classList.replace('has-text-dark','has-text-teal');
+            i.classList.replace('has-text-dark', 'has-text-teal');
         });
         document.cookie = 'colormode=dark; samesite=strict; expires=Wed, 01 Jan 2025 05:00:00 GMT;';
     }
     else if (document.cookie == 'colormode=dark') {
-        document.getElementById('sun-moon').classList.replace('has-text-teal','has-text-dark');
-        document.getElementById('sun-moon').classList.replace('fa-sun','fa-moon');
-        document.getElementById('html-background').classList.replace('has-dark-bkg','has-teal-bkg');
+        document.getElementById('sun-moon').classList.replace('has-text-teal', 'has-text-dark');
+        document.getElementById('sun-moon').classList.replace('fa-sun', 'fa-moon');
+        document.getElementById('html-background').classList.replace('has-dark-bkg', 'has-teal-bkg');
 
         let titles = document.getElementsByClassName('title');
         Array.from(titles).forEach(i => {
-            i.classList.replace('has-text-teal','has-text-dark');
+            i.classList.replace('has-text-teal', 'has-text-dark');
         });
 
         let subtitles = document.getElementsByClassName('subtitle');
         Array.from(subtitles).forEach(i => {
-            i.classList.replace('has-text-teal','has-text-dark');
+            i.classList.replace('has-text-teal', 'has-text-dark');
         });
 
         let icons = document.getElementsByClassName('fab');
         Array.from(icons).forEach(i => {
-            i.classList.replace('has-text-teal','has-text-dark');
+            i.classList.replace('has-text-teal', 'has-text-dark');
         });
         document.cookie = 'colormode=teal; samesite=strict; expires=Wed, 01 Jan 2025 05:00:00 GMT;';
     }
